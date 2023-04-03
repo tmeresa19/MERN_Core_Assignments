8f7274efa0810554cb576532c9d15a63e27280fe
Tigist Meresa <tmeresa19@gmail.com>
2023-04-03 10:33:26 -0700

    Initial commit

---
+++ Predict_Destructuring.js
@@ -0,0 +1,61 @@
+// Predict the Output for Problem 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [randomCar] = cars
+const [, otherRandomCar] = cars
+//Predict the output
+console.log(randomCar) //Tesla
+console.log(otherRandomCar)//Mercedes
+
+
+// Predict the Output for Problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name); //ReferenceError: because name is not defined in the current scope
+console.log(otherName); //'Elon'
+
+// Predict the Output for Problem 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;
+//Predict the output
+console.log(password); //'12345'
+console.log(hashedPassword); //undefined: since the person object does not have a property named password
+
+// Predict the Output for Problem 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [, first] = numbers; 
+const [, , , second] = numbers; 
+const [, , , , , , , , third] = numbers; 
+//Predict the output
+console.log(first == second);//false: because they have different values (2 and 5, respectively)
+console.log(first == third); //true: because they have the same value (2)
+
+// Predict the Output for Problem 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [, willThisWork] = secondKey;
+//Predict the output
+console.log(key); //'value'
+console.log(secondKey); //[1, 5, 1, 8, 3, 3]
+console.log(secondKey[0]); //1
+console.log(willThisWork); //??
+
+
+
+
+
+
+
